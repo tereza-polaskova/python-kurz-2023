581a5bc18a5f2ee3433230e4262fced5348aee46
Tereza Polášková <tereza.tobolicova@gmail.com>
2023-02-05 12:43:37 +0100

    úkol 1

--- ukol-1.py
+++ ukol-1.py
@@ -1,5 +1,8 @@
 # úkol 1
 
+# Napiš program, který bude obsahovat jednu proměnnou jmeno. Tato proměnná bude obsahovat libovolné křestní jméno (třeba tvoje). 
+# Tvůj program vytvoří e-mailovou adresu na základě této proměnné, s doménou czechitas.cz a tuto e-mailovou adresu vypíše.
+
 jmeno = "Tereza"
 
 print(jmeno + "@czechitas.cz")
